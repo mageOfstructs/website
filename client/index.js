b059a64c800f30446e5c806a82848cecac439270
mageOfStructs <jason.puschnig@gmx.at>
2025-03-18 23:42:26 +0100

    add log demo

---
+++ client/index.js
@@ -0,0 +1,93 @@
+function init() {
+	loadLog();
+}
+
+function loadLog() {
+	const req = new XMLHttpRequest();
+	req.open("GET", "/log.txt");
+	req.addEventListener("load", handleLogLoaded);
+	req.send();
+}
+
+let glob_text_off = 0;
+
+function getLine(str, off) {
+	if (!off) {
+		off = glob_text_off;
+		glob_text_off = str.indexOf("\n", off) + 1;
+	}
+	if (glob_text_off === 0) return "";
+	return str.substring(off, str.indexOf("\n", off));
+}
+
+let begin = window.performance.now();
+function parseLine(line) {
+	const ret = document.createElement("p");
+	ret.className = "font-mono text-stone-50";
+	let matches = /OK/.exec(line);
+	let last_stop = 0;
+	if (matches) {
+		ret.appendChild(
+			document.createTextNode(line.substring(last_stop, matches.index)),
+		);
+		const tmp = document.createElement("span");
+		tmp.textContent = "OK";
+		tmp.className = "text-green-600 font-bold";
+		ret.appendChild(tmp);
+		last_stop = matches.index + 2;
+	}
+
+	if ((matches = /FAILED/.exec(line))) {
+		ret.appendChild(
+			document.createTextNode(line.substring(last_stop, matches.index)),
+		);
+		const tmp = document.createElement("span");
+		tmp.textContent = "FAILED";
+		tmp.className = "text-red-600 font-bold";
+		ret.appendChild(tmp);
+		last_stop = matches.index + tmp.textContent.length;
+	}
+
+	if ((matches = /\<time\>/.exec(line))) {
+		ret.appendChild(
+			document.createTextNode(line.substring(last_stop, matches.index)),
+		);
+		const tmp = document.createElement("span");
+		console.log(window.performance.now());
+		console.log(begin);
+		tmp.textContent = (
+			window.performance.now() / 1000 -
+			begin / 1000
+		).toPrecision(6);
+		// tmp.className = "text-red-600 font-bold";
+		ret.appendChild(tmp);
+		last_stop = matches.index + 6;
+	}
+
+	if ((matches = /\(.*\)\*.*/.exec(line))) {
+		ret.appendChild(
+			document.createTextNode(line.substring(last_stop, matches.index)),
+		);
+		const tmp = document.createElement("span");
+		tmp.textContent = matches[0].substring(matches[0].indexOf("*") + 1);
+		tmp.className = matches[0].substring(1, matches[0].indexOf(")"));
+		ret.appendChild(tmp);
+		last_stop = matches.index + matches[0].length;
+	}
+
+	ret.appendChild(document.createTextNode(line.substring(last_stop)));
+	return ret;
+}
+
+async function handleLogLoaded() {
+	const xhr = this;
+	console.log(xhr.textContent);
+	const container = document.getElementById("logContainer");
+	container.innerHTML = "";
+	do {
+		const line = getLine(xhr.response);
+		console.log(line);
+		container.appendChild(parseLine(line));
+		await new Promise((r) => setTimeout(r, 50));
+	} while (glob_text_off !== 0);
+}
