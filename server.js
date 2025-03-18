29d4cca04e44b58a13f72e669c94b6c523e8c142
mageOfStructs <jason.puschnig@gmx.at>
2025-03-18 21:15:29 +0100

    skeleton framework

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
+
+const app = express();
+const port = 8080;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.json());
+app.use(express.static("client"));
+
+app.get("/test", (req, res) =>
+	res.end("Hello from Express! (yes yes this will be changed to Rust later)"),
+);
+
+app.listen(port, () => {
+	console.log(`Employees app listening on port ${port}`);
+});
