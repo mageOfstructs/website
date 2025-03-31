79d358c6280321ee1329d2780de57f6e271b2c69
mageOfstructs <jason.puschnig@gmx.at>
2025-03-31 20:34:48 +0200

    resolve mc

--- to_html.sh
+++ to_html.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+
+# codeblock() {
+#   IFS='`'
+#   CODEBLOCKS=$(grep test.md -zoe "\`\`\`.*\`\`\`")
+#   for i in $CODEBLOCKS; do 
+#     if [ -n "$(echo $i | tr -d ' \n')" ]; then 
+#       IFS='\n'
+#       CODEBLOCK_LANG=$($i[0])
+#       IFS='`'
+#       echo $i >> out
+#     fi
+#   done
+# }
+
 awk -f ./list.awk <$1 > "$2"
 # one sed command to rule them all
 sed -E -s "$2" -e "s/\*\*(.*)\*\*/<b>\1<\/b>/g" -e "s/\*(.*)\*/<i>\1<\/i>/g" -e "s/^- (.*)\$/<li>\1<\/li>/g" -e "/^#/d" > "$2"
