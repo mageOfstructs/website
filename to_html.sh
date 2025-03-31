12d2c222ffc3fba9181d50bb246e69f4513be970
mageOfstructs <jason.puschnig@gmx.at>
2025-03-31 21:33:17 +0200

    fix codeblock function

--- to_html.sh
+++ to_html.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-# codeblock() {
-#   IFS='`'
-#   CODEBLOCKS=$(grep test.md -zoe "\`\`\`.*\`\`\`")
-#   for i in $CODEBLOCKS; do 
-#     if [ -n "$(echo $i | tr -d ' \n')" ]; then 
-#       IFS='\n'
-#       CODEBLOCK_LANG=$($i[0])
-#       IFS='`'
-#       echo $i >> out
-#     fi
-#   done
-# }
+codeblock() {
+  IFS=$'`'
+  echo $IFS
+  filecnt=1
+  CODEBLOCKS=$(grep test.md -zoe "\`\`\`.*\`\`\`")
+  for i in $CODEBLOCKS; do 
+    if [ -n "$(echo $i | tr -d ' \n')" ]; then 
+      IFS=$'\n'
+      cb_lang=($i)
+      IFS=$'`'
+      echo "CBLANG: $cb_lang"
+      echo $(echo ${#cb_lang[@]}-1 | bc)
+      j=1
+      while [ $j -lt ${#cb_lang[@]} ]; do
+        echo "${cb_lang[$j]}" >> "out.$cb_lang"
+        j=$(($j + 1))
+      done
+      vim "out.$cb_lang" -c "TOhtml out$filecnt.html" -c "qa"
+      rm "out.$cb_lang"
+      filecnt=$(($filecnt + 1))
+    fi
+  done
+}
 
-awk -f ./list.awk <$1 > "$2"
 # one sed command to rule them all
-sed -E -s "$2" -e "s/\*\*(.*)\*\*/<b>\1<\/b>/g" -e "s/\*(.*)\*/<i>\1<\/i>/g" -e "s/^- (.*)\$/<li>\1<\/li>/g" -e "/^#/d" > "$2"
+cat $1 | awk -f ./list.awk | sed -E -e "s/\*\*(.*)\*\*/<b>\1<\/b>/g" -e "s/\*(.*)\*/<i>\1<\/i>/g" -e "s/^- (.*)\$/<li>\1<\/li>/g" -e "/^#/d" > "$2"
