ce5f2ef2872b1c23645aca6b696e6124473a6ca9
mageOfStructs <jason.puschnig@gmx.at>
2025-04-05 09:16:20 +0200

    fix codeblock function

--- to_html.sh
+++ to_html.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 readonly SCRIPTNAME="$0"
+readonly HEADING=$(/bin/grep "$1" -m1 -oPe "[^# ].*")
+readonly OUTFILE="./thoughts_gen/$HEADING.html"
+
 log() {
   echo "$SCRIPTNAME: " $1
 }
@@ -8,8 +11,9 @@ log() {
 codeblock() {
   IFS=$'`'
   echo $IFS
-  filecnt=1
-  CODEBLOCKS=$(grep test.md -zoe "\`\`\`.*\`\`\`")
+  filecnt=0
+  CODEBLOCKS=$(cat "$1" | awk -f ./codeblock_filter.awk)
+  mkdir -p "./dist/codeblocks/$HEADING"
   for i in $CODEBLOCKS; do 
     if [ -n "$(echo $i | tr -d ' \n')" ]; then 
       IFS=$'\n'
@@ -22,14 +26,15 @@ codeblock() {
         echo "${cb_lang[$j]}" >> "out.$cb_lang"
         j=$(($j + 1))
       done
-      vim "out.$cb_lang" -c "TOhtml out$filecnt.html" -c "qa"
+      vim --cmd "colorscheme elflord" "out.$cb_lang" -c "TOhtml" -c "w out$filecnt.html" -c "qa!"
       rm "out.$cb_lang"
+      mv "out$filecnt.html" "./dist/codeblocks/$HEADING/"
       filecnt=$(($filecnt + 1))
     fi
   done
 }
 
-readonly HEADING=$(/bin/grep "$1" -m1 -oPe "[^# ].*")
-readonly OUTFILE="./thoughts_gen/$HEADING.html"
+codeblock "$1"
+
 # one sed command to rule them all
-cat "$1" | awk -f ./tag_bounds.awk | sed -E -f ./tags.sed >> "$OUTFILE"
+cat "$1" | awk -f ./codeblock_replace.awk -v heading="$HEADING" | awk -f ./tag_bounds.awk | sed -E -f ./tags.sed >> "$OUTFILE"
