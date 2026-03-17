4edea3abb491c637e8da883ebf2c187f534ea1eb
mageOfStructs <mageOfStructs@proton.me>
2026-03-17 10:38:32 +0100

    fix test server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 
 const app = express();
-app.use("/", express.static("dist"));
+app.use("/", express.static("dist", { extensions: ["html", "htm"] }));
 
 app.listen(8080);
